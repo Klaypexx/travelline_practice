f6c16747462274fce957630e3c3e793a9cc1f5b4
justproggramer <morozow2121@mail.ru>
2024-03-18 18:47:50 +0300

    first commit
    Add homework - dictionary
    Add lecture 1

---
+++ lecture1/casino/Casino/Casino/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Programm
+{
+    static void Main()
+    {
+        double balance = 10000;
+        double multiplicator = 0.5;
+        Random rand = new Random();
+        while (balance != 0)
+        {
+            Console.WriteLine($"Введите вашу ставку. Ваш баланс сейчас {balance}");
+            string betText = Console.ReadLine();
+            int bet = int.Parse(betText);
+            if (bet <= balance)
+            {
+                int randomNumber = rand.Next(1, 20);
+                if (randomNumber == 18 || randomNumber == 19 || randomNumber == 20)
+                {
+                    double winSum = bet * (1 + (multiplicator * randomNumber % 17));
+                    balance += winSum;
+                    Console.WriteLine($"Поздравляем! Вы выиграли {winSum}");
+                }
+                else
+                {
+                    balance -= bet;
+                    Console.WriteLine("Вы проиграли");
+                }
+            }
+            else
+            {
+                Console.WriteLine("У вас нет столько денег для ставки");
+            }
+        }
+        Console.WriteLine("У вас не осталось денег...");
+    }
+}
---
+++ lecture1/test/test/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите название продукта: ");
+        string product = Console.ReadLine();
+
+        Console.Write("Введите количество: ");
+        string amountText = Console.ReadLine();
+        int amount = int.Parse(amountText);
+
+        Console.Write("Введите ваше имя: ");
+        string userName = Console.ReadLine();
+
+        Console.Write("Введите ваш адрес: ");
+        string adress = Console.ReadLine();
+
+        DateTime dateTime = DateTime.Now;
+
+        string firstOutputText = $"Здравствуйте, {userName}, вы заказали {amount} {product} на адрес {adress}, все верно?";
+        string secondOuputText = $"{userName}!\nВаш заказ {product} в количестве {amount} оформлен! \nОжидайте доставку по адресу {adress} к {dateTime.AddDays(3)}";
+
+        Console.WriteLine(firstOutputText);
+        string isCorrect = Console.ReadLine();
+        if (isCorrect == "Yes" || isCorrect == "yes" || isCorrect == "Да" || isCorrect == "да" || isCorrect == "")
+        {
+            Console.WriteLine(secondOuputText);
+        }
+    }
+}
