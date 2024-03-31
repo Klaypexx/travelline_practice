623d24889caf35ee6d213a6ccdc21c8e33f83184
justproggramer <morozow2121@mail.ru>
2024-03-31 17:33:39 +0300

    Gladiators

---
+++ lecture2(21.03)/Gladiators/Models/Armors/IArmor.cs
@@ -0,0 +1,10 @@
+﻿namespace Gladiators.Models.Armors
+{
+    public interface IArmor
+    {
+        public string Name { get; }
+        public int Armor { get; }
+
+
+    }
+}
---
+++ lecture2(21.03)/Gladiators/Models/Races/Lizard.cs
@@ -0,0 +1,14 @@
+﻿namespace Gladiators.Models.Races
+{
+    public class Lizard : IRace
+    {
+        public string Name { get; } = "Lizard";
+        public int Damage { get; } = 5;
+
+        public int Health { get; } = 150;
+
+        public int Armor { get; } = 15;
+
+        public int Initiative { get; } = 4;
+    }
+}
