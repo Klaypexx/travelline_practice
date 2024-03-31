3512fe283cddbb1e78a7cd46c30da82e076e7851
justproggramer <morozow2121@mail.ru>
2024-03-31 17:40:53 +0300

    casino + delivery

---
+++ lecture1(14.03)/casino/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Programm
+{
+    static void Main()
+    {
+        int balance = 10000;
+        const int multiplicator = 2;
+        Random rand = new Random();
+        while (balance != 0)
+        {
+            Console.WriteLine($"Введите вашу ставку. Ваш баланс сейчас {balance}");
+            string betText = Console.ReadLine();
+            if (!string.IsNullOrEmpty(betText)) {
+                if (!int.TryParse(betText, out int bet))
+                {
+                    Console.WriteLine("Ошибка: Введите корректное целое число для ставки.");
+                    continue;
+                }
+                if (bet <= balance)
+                {
+                    int randomNumber = rand.Next(1, 20);
+                    if (randomNumber == 18 || randomNumber == 19 || randomNumber == 20)
+                    {
+                        int winSum = bet * (1 + (multiplicator * randomNumber % 17));
+                        balance += winSum;
+                        Console.WriteLine($"Поздравляем! Вы выиграли {winSum}");
+                    }
+                    else
+                    {
+                        balance -= bet;
+                        Console.WriteLine("Вы проиграли");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("У вас нет столько денег для ставки");
+                }
+            } else
+            {
+                Console.WriteLine("Вы ввели пустое значение ставки");
+            }
+        }
+        Console.WriteLine("У вас не осталось денег...");
+    }
+}
---
+++ lecture1(14.03)/delivery/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Введите название продукта: ");
+        string productName = Console.ReadLine();
+
+        Console.Write("Введите количество: ");
+        string amountOfProduct = Console.ReadLine();
+
+        int amount;
+
+        try
+        {
+            amount = int.Parse(amountOfProduct);
+        } catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return;
+        }
+
+        Console.Write("Введите ваше имя: ");
+        string userName = Console.ReadLine();
+
+        Console.Write("Введите ваш адрес: ");
+        string adress = Console.ReadLine();
+
+        DateTime currentDate = DateTime.Now;
+        DateTime deliveryDate = currentDate.AddDays(3);
+
+        string confirmationText = $"Здравствуйте, {userName}, вы заказали {amount} {productName} на адрес {adress}, все верно?\nДля подтверждения напишите - да";
+        string deliveryInformationText = $"{userName}!\nВаш заказ {productName} в количестве {amount} оформлен! \nОжидайте доставку по адресу {adress} к {deliveryDate}";
+
+        Console.WriteLine(confirmationText);
+        string isCorrect = Console.ReadLine().Trim().ToLower();
+
+        if (isCorrect == "да")
+        {
+            Console.WriteLine(deliveryInformationText);
+        } else
+        {
+            Console.WriteLine("Заказ отменен");
+        }
+    }
+}
