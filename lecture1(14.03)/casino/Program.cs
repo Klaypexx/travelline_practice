e5da8ea36c872a4ee127a5a4c459112b7e3a2a7a
justproggramer <morozow2121@mail.ru>
2024-04-02 22:41:29 +0300

    resolve fix casino

--- lecture1(14.03)/casino/Program.cs
+++ lecture1(14.03)/casino/Program.cs
@@ -5,38 +5,41 @@
         int balance = 10000;
         const int multiplicator = 2;
         Random rand = new Random();
+
         while (balance != 0)
         {
             Console.WriteLine($"Введите вашу ставку. Ваш баланс сейчас {balance}");
             string betText = Console.ReadLine();
-            if (!string.IsNullOrEmpty(betText)) {
-                if (!int.TryParse(betText, out int bet))
-                {
-                    Console.WriteLine("Ошибка: Введите корректное целое число для ставки.");
-                    continue;
-                }
-                if (bet <= balance)
-                {
-                    int randomNumber = rand.Next(1, 20);
-                    if (randomNumber == 18 || randomNumber == 19 || randomNumber == 20)
-                    {
-                        int winSum = bet * (1 + (multiplicator * randomNumber % 17));
-                        balance += winSum;
-                        Console.WriteLine($"Поздравляем! Вы выиграли {winSum}");
-                    }
-                    else
-                    {
-                        balance -= bet;
-                        Console.WriteLine("Вы проиграли");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("У вас нет столько денег для ставки");
-                }
-            } else
+
+            if (string.IsNullOrEmpty(betText))
             {
                 Console.WriteLine("Вы ввели пустое значение ставки");
+                continue;
+            }
+
+            if (!int.TryParse(betText, out int bet))
+            {
+                Console.WriteLine("Ошибка: Введите корректное целое число для ставки.");
+                continue;
+            }
+
+            if (bet > balance)
+            {
+                Console.WriteLine("У вас нет столько денег для ставки");
+                continue;
+            }
+
+            int randomNumber = rand.Next(1, 20);
+            if (randomNumber == 18 || randomNumber == 19 || randomNumber == 20)
+            {
+                int winSum = bet * (1 + (multiplicator * randomNumber % 17));
+                balance += winSum;
+                Console.WriteLine($"Поздравляем! Вы выиграли {winSum}");
+            }
+            else
+            {
+                balance -= bet;
+                Console.WriteLine("Вы проиграли");
             }
         }
         Console.WriteLine("У вас не осталось денег...");
