545f4da7e0d3e352e041c02e837913e4dae85aec
justproggramer <morozow2121@mail.ru>
2024-05-19 00:33:55 +0300

    commit

--- TypeScript/1-lab-tools-main/src/lib/compareJsons.ts
+++ TypeScript/1-lab-tools-main/src/lib/compareJsons.ts
@@ -1,7 +1,5 @@
 "use strict";
 import fs from 'fs';
-
-
 type Compare = Record<string, CompareValues>;
 type CompareValues = {
   type: `unchanged` | `new` | `changed` | `delete`,
@@ -17,23 +15,55 @@ type JsonObject = {
 };
 
 const compareObjects = (oldObject: JsonObject, newObject: JsonObject): Compare  => {
-  const allKeys: string[] = Array.from(new Set([...Object.keys(oldObject), ...Object.keys(newObject)]));
+  const keys: string[] = Array.from(new Set([...Object.keys(oldObject), ...Object.keys(newObject)]));
 
-  return allKeys.reduce<Compare>((result, key) => {
-    // console.log(key);
+  return keys.reduce<Compare>((result, key) => {
     const oldObjVal = oldObject[key];
     const newObjVal = newObject[key];
 
-    if (oldObjVal != undefined && newObjVal != undefined) {
-      console.log(oldObjVal);
-      console.log(newObjVal);
+    if (!(oldObject.hasOwnProperty(key))) {
+      return {
+        ...result,
+        [key]: {
+          type: 'new',
+          newValue: newObjVal
+        }
+      }
     }
-    return result;
+
+    if (!(newObject.hasOwnProperty(key))) {
+      return {
+        ...result,
+        [key]: {
+          type: 'delete',
+          oldObject: oldObjVal
+        }
+      }
+    }
+
+    if (typeof oldObjVal === 'object' && typeof newObjVal === 'object') {
+      return {
+        ...result,
+        [key]: {
+          type: 'changed',
+          children: compareObjects(oldObjVal as JsonObject, newObjVal as JsonObject)
+        }
+      }
+    }
+
+    return {
+      ...result,
+      [key]: {
+        type: oldObjVal == newObjVal ? 'unchanged' : 'changed',
+        oldValue: oldObjVal,
+        newValue: newObjVal
+      }
+    }
   }, {})
 }
 export const compareJsons = async (oldJsonPath: string, newJsonPath: string) => {
   const oldData = JSON.parse(fs.readFileSync(oldJsonPath, 'utf8'));
   const newData = JSON.parse(fs.readFileSync(newJsonPath, 'utf8'));
   const jsonDifference = compareObjects(oldData, newData);
-  console.log(jsonDifference);
+  console.log(JSON.stringify(jsonDifference, null, 2));
 }
